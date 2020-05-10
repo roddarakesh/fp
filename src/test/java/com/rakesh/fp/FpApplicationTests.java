8a03a847a19da933078f2f39f20ccb67521d9ea5
rakesh rr <rakesh.rodda@gmail.com>
2020-05-10 10:48:55 -0700

    first commit

---
+++ src/test/java/com/rakesh/fp/FpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.fp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
